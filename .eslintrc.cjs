3ea856f603daeccf329b5d5af273e8796393a444
Max Atslega <max@atslega.network>
2022-10-04 20:22:49 +0200

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,17 +5,46 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'plugin:prettier/recommended'
   ],
   overrides: [
   ],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect'
+    },
+    "import/resolver": {
+      "node": {
+        "paths": ["src"],
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      },
+      "alias": {
+        "extensions": [".ts", ".tsx"],
+        "map": [
+          ["@", "./src/"]
+        ]
+      }
+    }
+  },
   parserOptions: {
+    project: ["./tsconfig.json"],
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: [
-    'react'
+    'react',
+    'prettier'
   ],
   rules: {
+    "semi": ["error", "never"],
+    "prettier/prettier": ["error"],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-multiple-empty-lines': 'error',
+    "no-multi-spaces": "error",
+    "no-console": "error",
+    "import/no-unresolved": "error",
   }
 }
